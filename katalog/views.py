389c5d26b7df0a2c44c16b2c7a8f1e26d22c22bd
michaelsinanta <michaelchristlambert@gmail.com>
2022-09-13 11:22:26 +0700

    Mengerjakan nomor 1-4

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_katalog = CatalogItem.objects.all()
+    context = {
+        'list_katalog': data_katalog,
+        'nama': 'Michael Christlambert Sinanta',
+        'studentId': "2106750414"
+    }
+    return render(request, "katalog.html", context)
